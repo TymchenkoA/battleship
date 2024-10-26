eed70e6281867085a9efe0df7cf50b5a6ef42ff7
Anastasiia Tymchenko <tymchenko.anastasiia@gmail.com>
2024-10-26 01:44:07 +0100

    feat: implement user sign in and sign up

--- src/ws_server/index.js
+++ src/ws_server/index.js
@@ -1,4 +1,5 @@
 import { WebSocketServer } from "ws";
+import usersDB from "../db/users.js";
 
 export function startWsServer(wsServerPort) {
   const wsServer = new WebSocketServer({ port: wsServerPort });
@@ -8,24 +9,59 @@ export function startWsServer(wsServerPort) {
 
     socket.on("message", (message) => {
       const receivedData = JSON.parse(message);
-      console.log(receivedData);
 
       if (receivedData.type === "reg") {
         const { name, password } = JSON.parse(receivedData.data);
-        console.log(
-          `Received login data - Username: ${name}, Password: ${password}`
-        );
 
-        // socket.send(JSON.stringify({
-        //   type: 'reg',
-        //   data: {
-        //     name: name,
-        //     index: 1,
-        //     error: false,
-        //     errorText: ""
-        //   },
-        //   id: 0
-        // }));
+        if (!name || !password) {
+          socket.send(
+            JSON.stringify({
+              type: "reg",
+              data: JSON.stringify({
+                name: "",
+                index: null,
+                error: true,
+                errorText: "Name and password are required",
+              }),
+              id: 0,
+            })
+          );
+
+          return;
+        }
+
+        const registrationResult = usersDB.registerUser(name, password);
+
+        if (registrationResult.error) {
+          socket.send(
+            JSON.stringify({
+              type: "reg",
+              data: JSON.stringify({
+                name,
+                index: registrationResult.index || null,
+                error: true,
+                errorText: registrationResult.errorText,
+              }),
+              id: 0,
+            })
+          );
+        } else {
+          const newUser = registrationResult.user;
+
+          console.log(newUser);
+          socket.send(
+            JSON.stringify({
+              type: "reg",
+              data: JSON.stringify({
+                name: newUser.name,
+                index: newUser.id,
+                error: false,
+                errorText: "",
+              }),
+              id: 0,
+            })
+          );
+        }
       }
     });
 
@@ -33,6 +69,6 @@ export function startWsServer(wsServerPort) {
       console.log("WebSocket connection closed");
     });
   });
-  
+
   console.log(`WebSocket server is running on ws://localhost:${wsServerPort}`);
 }
